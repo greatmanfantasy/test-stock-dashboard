bde01749def2869598653470c96341b5265551a2
INGUN HWANG <greatmanfantasy@gmail.com>
2025-04-24 15:58:13 +0900

    .

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from rsi import calculate_rsi
 from chart import plot_chart
 from utils import handle_file_upload
 from pycoingecko import CoinGeckoAPI
+from datetime import datetime, timedelta
 
 def get_bitcoin_price():  # 이 함수가 없으면 에러 발생
     cg = CoinGeckoAPI()
